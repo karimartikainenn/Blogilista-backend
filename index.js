a790edcd527ffc98eb86cc9367af38719204c236
Kari <kari_martikainen@icloud.com>
2024-03-02 23:29:04 +0200

    app.js tehty

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./app')
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const blogsRouter = require("./controllers/blogs");
-const Blog = require('./models/blog');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const mongoUrl = process.env.MONGO_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
