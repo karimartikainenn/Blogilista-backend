ae6a09f14ad065da5f5ebe3ba894d4658f826946
Kari <kari_martikainen@icloud.com>
2024-03-02 23:21:22 +0200

    controllers and models tehty

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require("dotenv").config()
+const express = require('express');
+const app = express();
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require("./controllers/blogs");
+const Blog = require('./models/blog');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const mongoUrl = process.env.MONGO_URI;
+mongoose.connect(mongoUrl);
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+app.use(cors());
+app.use(express.json());
 
-const Blog = mongoose.model('Blog', blogSchema)
+app.use('/api/blogs', blogsRouter);
 
-const mongoUrl = process.env.MONGO_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
