eeda2a36aecc758dfbf35e38e30b9cb602278d56
Kari <kari_martikainen@icloud.com>
2024-03-03 12:40:13 +0200

    apufunktioita ja yksikkötestejä, step3

---
+++ tests/favoriteBlog.test.js
@@ -0,0 +1,53 @@
+const { test, describe } = require('node:test');
+const assert = require('node:assert');
+const listHelper = require('../utils/list_helper');
+
+describe('favorite blog', () => {
+  test('when list has only one blog, return the blog', () => {
+    const listWithOneBlog = [
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    ];
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    });
+  });
+
+  test('when list has multiple blogs, returns the blog with most likes', () => {
+    const listWithMultipleBlogs = [
+      {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        likes: 7
+      },
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      },
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12
+      }
+    ];
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs);
+    assert.deepStrictEqual(result, {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    });
+  });
+
+  test('when list is empty, returns null', () => {
+    const emptyList = [];
+    const result = listHelper.favoriteBlog(emptyList);
+    assert.strictEqual(result, null);
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 0) {
+        return null; // Jos taulukko on tyhjä palautetaan null
+    }
+    
+    const maxLikesBlog = blogs.reduce((maxBlog, currentBlog) => {
+        return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog;
+    })
+
+    return {
+        title: maxLikesBlog.title,
+        author: maxLikesBlog.author,
+        likes: maxLikesBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
