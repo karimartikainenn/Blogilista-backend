8b1391a5b337251b8bd91701c31e84233f8468fc
Kari <kari_martikainen@icloud.com>
2024-03-05 11:47:15 +0200

    blogilistan testit, step 1 valmis

---
+++ mongo.js
@@ -0,0 +1,11 @@
+test("These are two blogs", async () => {
+  const response = await api.get("/api/blogs");
+  assert.strictEqual(response.body.length, 2);
+});
+
+test("The first blog is about HTTP methods", async () => {
+    const response = await api.get("/api/blogs");
+
+    const contents = response.body.map(r => r.title);
+    assert.strictEqual(contents.includes("HTTP methods"), true);
+});
