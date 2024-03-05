69a9d2f68520176afe8d1f67c914670cd3175136
Kari <kari_martikainen@icloud.com>
2024-03-05 17:06:14 +0200

    blogilistan laajennus, step6 valmis

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const loginRouter = require("./controllers/login");
 const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 const cors = require("cors");
@@ -9,6 +10,7 @@ const mongoUrl = process.env.MONGO_URI;
 mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
+app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
