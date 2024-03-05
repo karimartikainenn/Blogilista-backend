a7824324424dc1e1671a6abf64e20c79494835d5
Kari <kari_martikainen@icloud.com>
2024-03-05 16:51:11 +0200

    jsonwebtoken asennettu

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,39 +1,37 @@
-const blogsRouter = require("express").Router()
-const Blog = require("../models/blog")
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+blogsRouter.get("/", (request, response) => {
+  Blog.find({})
+    .populate("user", { username: 1, name: 1 })
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
 
-  blogsRouter.delete('/:id', async (request, response) => {
-    const id = request.params.id;
-  
-    try {
-      const deletedBlog = await Blog.findByIdAndDelete(id);
-  
-      if (!deletedBlog) {
-        return response.status(404).json({ error: 'Blog not found' });
-      }
-  
-      response.status(204).end();
-    } catch (error) {
-      console.error(error);
-      response.status(500).json({ error: 'Internal Server Error' });
-    }
+blogsRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
   });
+});
 
-module.exports = blogsRouter
+blogsRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+
+    if (!deletedBlog) {
+      return response.status(404).json({ error: "Blog not found" });
+    }
+
+    response.status(204).end();
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = blogsRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,12 @@ const usersRouter = require("express").Router();
 const User = require("../models/users");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    url: 1,
+    author: 1,
+    likes: 1,
+  });
   response.json(users);
 });
 
