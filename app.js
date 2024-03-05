808117147f4d6a80dea51f455ec6647e30ef2bc4
Kari <kari_martikainen@icloud.com>
2024-03-05 16:32:27 +0200

    blogilistan laajennus, step3 Valmis!

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
+const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const mongoUrl = process.env.MONGO_URI;
 mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
-app.use('/api/blogs', blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
